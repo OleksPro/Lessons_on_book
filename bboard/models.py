e8c01e8266452ecc5d0054b2feef9b04a3474a03
OlexPro <oleks.prokopenko92@gmail.com>
2024-03-27 12:36:22 +0200

    worked with forms, jinja. added pages for create posts

--- bboard/models.py
+++ bboard/models.py
@@ -1,12 +1,39 @@
 from django.db import models
 
+
+class Rubric(models.Model):
+    name = models.CharField(
+        max_length=20, 
+        db_index=True, 
+        verbose_name="Назва"
+        )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Рубрики'
+        verbose_name = 'Рубрика'
+        ordering = ['name']
+
+
 class Bb(models.Model):
     title = models.CharField(max_length=50, verbose_name='Товар')
     content = models.TextField(null=True, blank=True, verbose_name='Опис')
     price = models.FloatField(null=True, blank=True, verbose_name='Ціна')
-    published = models.DateTimeField(auto_now_add=True, db_index=True, verbose_name='Дата публікації')
+    published = models.DateTimeField(
+        auto_now_add=True, db_index=True, 
+        verbose_name='Дата публікації'
+    )
+
+    rubric = models.ForeignKey('Rubric', 
+        null=True, 
+        on_delete=models.PROTECT, 
+        verbose_name='Рубрика'
+        )
 
     class Meta:
+        verbose_name_plural = 'Оголошення'
         verbose_name = 'Оголошення'
         ordering = ['-published']
     
